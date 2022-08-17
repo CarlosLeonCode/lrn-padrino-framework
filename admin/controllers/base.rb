cec65fb07f9febd0cf0da95c7c31b54b52cc6629
Carlos Leon <carlosestebanleonpinilla@gmail.com>
2022-08-17 16:27:27 -0500

    padrino demo app base ok

---
+++ admin/controllers/base.rb
@@ -0,0 +1,5 @@
+LrnPadrino::Admin.controllers :base do
+  get :index, :map => "/" do
+    render "base/index"
+  end
+end
---
+++ config/apps.rb
@@ -0,0 +1,46 @@
+##
+# This file mounts each app in the Padrino project to a specified sub-uri.
+# You can mount additional applications using any of these commands below:
+#
+#   Padrino.mount('blog').to('/blog')
+#   Padrino.mount('blog', :app_class => 'BlogApp').to('/blog')
+#   Padrino.mount('blog', :app_file =>  'path/to/blog/app.rb').to('/blog')
+#
+# You can also map apps to a specified host:
+#
+#   Padrino.mount('Admin').host('admin.example.org')
+#   Padrino.mount('WebSite').host(/.*\.?example.org/)
+#   Padrino.mount('Foo').to('/foo').host('bar.example.org')
+#
+# Note 1: Mounted apps (by default) should be placed into the project root at '/app_name'.
+# Note 2: If you use the host matching remember to respect the order of the rules.
+#
+# By default, this file mounts the primary app which was generated with this project.
+# However, the mounted app can be modified as needed:
+#
+#   Padrino.mount('AppName', :app_file => 'path/to/file', :app_class => 'BlogApp').to('/')
+#
+
+##
+# Setup global project settings for your apps. These settings are inherited by every subapp. You can
+# override these settings in the subapps as needed.
+#
+Padrino.configure_apps do
+  # enable :sessions
+  set :session_secret, '45dc4262f42bf8cb08e905799d2aaa7f2bcd5bbb02a857de6dff69757424f6d5'
+  set :protection, :except => :path_traversal
+  set :protect_from_csrf, true
+
+  if RACK_ENV == 'production'
+    disable :reload
+    disable :reload_templates
+  else
+    enable :reload
+    enable :reload_templates
+  end
+end
+
+# Mounts the core application for this project
+
+Padrino.mount("LrnPadrino::Admin", :app_file => Padrino.root('admin/app.rb')).to("/admin")
+Padrino.mount('LrnPadrino::App', :app_file => Padrino.root('app/app.rb')).to('/')
